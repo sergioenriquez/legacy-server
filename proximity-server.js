f2440d9812f00d09cc0595bf213da663845bbc9d
Sergio Enriquez <sergioe619@gmail.com>
2016-03-29 23:02:00 -0700

    testing boostrap styles

--- proximity-server.js
+++ proximity-server.js
@@ -1,4 +1,4 @@
-People = new Mongo.Collection("people");
+Beacons = new Mongo.Collection("beacons");
 
 if (Meteor.isClient) {
   // counter starts at 0
@@ -7,15 +7,24 @@ if (Meteor.isClient) {
   Template.beacon.helpers({
     made(){
       return this.date;
+    },
+    created(){
+      return this.created.toISOString();
+    },
+    lastSeen(){
+      if(!this.updated){
+        return null;
+      }
+      return moment(this.updated).fromNow();
     }
   });
 
   Template.beacon.events({
     'click .delete': function(){
-      People.remove(this._id);
+      Beacons.remove(this._id);
     },
     'click .toggle-checked': function () {
-      People.update(this._id, {$set: {is_beek: !this.is_beek}});
+      Beacons.update(this._id, {$set: {is_beek: !this.is_beek}});
     }
   });
 
@@ -23,30 +32,37 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     counter: function () {
-      return People.find().count();
+      return Beacons.find().count();
     },
     beacons: function(){
-      return People.find();
+      return Beacons.find();
     },
     foo(){
-      return moment(this.date).fromNow();
+      return moment(this.updated).fromNow();
     }
   });
 
   Template.body.events({
-    'click button': function () {
+    'click button-primary': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     },
 
-    'submit .new-beacon': function(event){
-      let name = event.target.UUID.value;
-      People.insert({
+    'submit .form-inline': function(event){
+      let name = event.target.name.value;
+      let mac = event.target.MAC.value;
+      let uuid = event.target.UUID.value;
+      Beacons.insert({
         name: name,
-        date: new Date(),
+        mac: mac,
+        uuid: uuid,
+        created: new Date(),
+        updated: null,
         is_beek: true,
         rssi: 0
       });
+      event.target.name.value = "";
+      event.target.mac.value = "";
       event.target.UUID.value = "";
       return false;
     }
