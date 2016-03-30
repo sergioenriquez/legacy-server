04cf924c2882bd3e735b9c69d03af58d4aa78b07
Sergio Enriquez <sergioe619@gmail.com>
2016-03-29 20:48:17 -0700

    First DB test

--- proximity-server.js
+++ proximity-server.js
@@ -4,11 +4,28 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 10);
 
-  Template.main.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.beacon.helpers({
+    made(){
+      return this.date;
+    }
+  });
+
+  Template.beacon.events({
+    'click .delete': function(){
+      People.remove(this._id);
     },
-    people: function(){
+    'click .toggle-checked': function () {
+      People.update(this._id, {$set: {is_beek: !this.is_beek}});
+    }
+  });
+
+  ///////////////////////////
+
+  Template.body.helpers({
+    counter: function () {
+      return People.find().count();
+    },
+    beacons: function(){
       return People.find();
     },
     foo(){
@@ -16,10 +33,22 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.main.events({
+  Template.body.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
+    },
+
+    'submit .new-beacon': function(event){
+      let name = event.target.UUID.value;
+      People.insert({
+        name: name,
+        date: new Date(),
+        is_beek: true,
+        rssi: 0
+      });
+      event.target.UUID.value = "";
+      return false;
     }
   });
 }
